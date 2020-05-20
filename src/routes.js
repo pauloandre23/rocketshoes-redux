e4861c0dd50c9412cac35e4a1be84c99f9d675a6
paulo meu filho <pauloandre232@gmail.com>
2020-05-19 21:22:22 -0300

    added cart reducer to the project

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+ 
+
+import Home from './pages/Home/index';
+import Cart from './pages/Cart/index';
+
+export default function Routes(){
+    return(
+        <Switch>
+           
+            <Route path='/' exact component = {Home}/>
+            <Route path='/cart' component = {Cart}/>
+        </Switch>
+    );
+    
+}
